92c7fc079c56d624f5dd59d35c608902fdc03fc6
Marc Juchli <mail@marcjuch.li>
2015-11-06 23:05:15 +0100

    xml structure draft

--- Dash.hs
+++ Dash.hs
@@ -1,21 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Dash (
   MPD (..),
-  Period (..)
+  Period (..),
+  AdaptationSet(..),
+  Representation(..),
+  SegmentTemplate(..),
+  Periodable(..)
 ) where
 
 import qualified Data.Text as T
+import qualified Data.Time.Clock                    as UTC
+import Text.XML.Generator
 
-data MPD = MPD { mpdId :: Int}
+data MPD = MPD {  mpdId :: Int
+                , mpdPeriods :: [Period]
+                , availabilityStartTime :: UTC.UTCTime
+              }
 
 data Period = Period { perId          :: Int,
-                       perStart       :: T.Text,
-                       perDuration    :: Int,
-                       perAdaptionSet :: AdaptationSet
+                       perStart       :: UTC.UTCTime,
+                       perAdaptionSets :: [AdaptationSet]
                      }
 
 data AdaptationSet = AdaptationSet { asMimeType       :: T.Text,
                                      asCodecs         :: T.Text,
-                                     asRepresentation :: Representation
+                                     asRepresentations :: [Representation]
                                    }
 data Representation = Representation { repId              :: T.Text,
                                        repBandwidth       :: Int,
@@ -28,3 +38,49 @@ data SegmentTemplate = SegmentTemplate { stTimescale      :: Int,
                                          stMedia          :: T.Text,
                                          stInitialization :: T.Text
                                        }
+
+type PeriodId = Int
+class Periodable a where
+  toPeriod :: a -> PeriodId -> Int -> Period
+
+showT s = (T.pack . show) s
+
+class XmlConvertible t where
+  toXml :: t -> Xml Elem
+
+instance XmlConvertible MPD where
+  toXml m = xelem "MPD" $
+                 xattr "xmlns:xsi" "http://www.w3.org/2001/XMLSchema-instance"
+              <> xattr "xmlns" "urn:mpeg:dash:schema:mpd:2011"
+              <> xattr "xsi:schemaLocation" "urn:mpeg:dash:schema:mpd:2011 http://standards.iso.org/ittf/PubliclyAvailableStandards/MPEG-DASH_schema_files/DASH-MPD.xsd"
+              <> xattr "type" "dynamic"
+              <> xattr "availabilityStartTime" ((showT . availabilityStartTime) m)
+              <> xattr "timeShiftBufferDepth" "PT10M"
+              <> xattr "minBufferTime" "PT10S"
+              <> xattr "profiles" "urn:mpeg:dash:profile:isoff-live:2011"
+              <> xattr "minimumUpdatePeriod" "PT2S"
+
+instance XmlConvertible Period where
+  toXml p = xelem "Period" $
+                  xattr "id" ((showT . perId) p)
+              <>  xattr "start" ((show . perStart) p)
+              <#> map toXml (perAdaptionSets p)
+
+
+instance XmlConvertible AdaptationSet where
+  toXml as = xelem "AdaptationSet" $
+                 xattr "mimeType" ((showT . asMimeType) as)
+              <> xattr "codecs" ((showT . asCodecs) as)
+             <#> map toXml (asRepresentations as)
+
+instance XmlConvertible Representation where
+  toXml r = xelem "Representation" $
+                 xattr "id" ((showT . repId) r)
+              <> xattr "bandwidth" ((showT . repBandwidth) r)
+             <#> toXml (repSegmentTemplate r)
+
+instance XmlConvertible SegmentTemplate where
+  toXml st = xelem "SegmentTemplate" $
+                 xattr "timescale" ((showT . stTimescale) st)
+              <> xattr "duration" ((showT . stDuration) st)
+              <> xattr "startNumber" ((showT . stStartNumber) st)
--- Main.hs
+++ Main.hs
@@ -36,18 +36,28 @@ instance QueryResults CampaignInfo where
               mpd = convert ff vf
     convertResults fs vs  = convertError fs vs 6
 
+
+instance Periodable CampaignInfo where
+  toPeriod a i d = Period i (start a) $
+                      AdaptationSet "video/mp4" "avc1.42c00d" $
+                          Representation "272p800kbps" 800000 $
+                                SegmentTemplate 24000 d 0 "video_0_800000/dash/segment_$Number$.m4s" "video_0_800000/dash/init.mp4"
+
+
 getDisplays :: Connection -> IO [Display]
 getDisplays conn = query_ conn "select * from display"
 
 findCampaignInfo :: Connection -> IO [CampaignInfo]
 findCampaignInfo conn = query_ conn
-        "select c.id, c.display_id, c.start, c.end, m.src, m.mpd_url from campaign c left join media m on m.id = c.media_id"
+        "select c.id, c.display_id, c.start, c.end, m.src, m.mpd_url from campaign c left join media m on m.id = c.media_id where c.start >= now()"
 
-filterNoMpd = filter (\ci -> isJust $ mpd ci )
+filterOnlyFirstN = take 10
+
+filterNoMpd = filter (isJust . mpd)
 --filterStartEnd cis now = filter (\ci -> (start ci >= now) && (end ci <= now)) cis
 
 filterCampaignInfos :: [CampaignInfo] -> [CampaignInfo]
-filterCampaignInfos = filterNoMpd  -- . filterStartEnd
+filterCampaignInfos = filterNoMpd  . filterOnlyFirstN
 
 createPeriod :: CampaignInfo -> [Period]
 createPeriod ci = []
