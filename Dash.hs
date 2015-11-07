99d2fe6c6a1993493c70739133b065c19f0a0d7b
Marc Juchli <mail@marcjuch.li>
2015-11-07 15:39:46 +0100

    default videoformats and profiles

--- Dash.hs
+++ Dash.hs
@@ -6,31 +6,47 @@ module Dash (
   AdaptationSet(..),
   Representation(..),
   SegmentTemplate(..),
-  Periodable(..)
+  Periodable(..),
+  defaultVideoFormat,
+  videoProfile2160p,
+  videoProfile1080p,
+  videoProfile720p
 ) where
 
-import qualified Data.Text as T
-import qualified Data.Time.Clock                    as UTC
-import Text.XML.Generator
+import qualified Data.Text          as T
+import qualified Data.Time.Clock    as UTC
+import           Data.Time.Format
+import           Text.XML.Generator
 
-data MPD = MPD {  mpdId :: Int
-                , mpdPeriods :: [Period]
-                , availabilityStartTime :: UTC.UTCTime
+
+data MPD = MPD {  mpdId                 :: Int
+                , mpdPeriods            :: [Period]
+                , availabilityStartTime :: T.Text
               }
 
-data Period = Period { perId          :: Int,
-                       perStart       :: UTC.UTCTime,
+data Period = Period { perId           :: Int,
+                       perStart        :: T.Text,
                        perAdaptionSets :: [AdaptationSet]
                      }
 
-data AdaptationSet = AdaptationSet { asMimeType       :: T.Text,
-                                     asCodecs         :: T.Text,
-                                     asRepresentations :: [Representation]
+data AdaptationSet = AdaptationSet { asVideoFormat     :: VideoFormat
+                                   , asRepresentations :: [Representation]
                                    }
-data Representation = Representation { repId              :: T.Text,
-                                       repBandwidth       :: Int,
-                                       repSegmentTemplate :: SegmentTemplate
+
+data VideoFormat = VideoFormat { vfMimeType :: T.Text
+                               , vfCodecs   :: T.Text
+                               }
+
+data Representation = Representation { repVideoProfile :: VideoProfile
+                                     , repSegmentTemplate :: SegmentTemplate
                                      }
+
+data VideoProfile = VideoProfile { vpId        :: T.Text,
+                                   vpBandwith  :: Int,
+                                   vpWidth     :: Int,
+                                   vpHeight    :: Int,
+                                   vpFrameRate :: Int
+                                 }
 
 data SegmentTemplate = SegmentTemplate { stTimescale      :: Int,
                                          stDuration       :: Int,
@@ -43,7 +59,36 @@ type PeriodId = Int
 class Periodable a where
   toPeriod :: a -> PeriodId -> Int -> Period
 
-showT s = (T.pack . show) s
+showT :: (Show s) => s -> T.Text
+showT = T.pack . show
+
+iso8601Duration :: UTC.UTCTime -> T.Text
+iso8601Duration t = T.pack $ "P" ++ formatTime defaultTimeLocale "%s" t
+
+defaultVideoFormat = VideoFormat { vfMimeType = "video/mp4"
+                                 , vfCodecs = "avc1.42c00d"
+                                 }
+
+videoProfile2160p = VideoProfile { vpId = "2160p12Mbps"
+                                , vpBandwith = 12000
+                                , vpWidth = 4096
+                                , vpHeight = 2160
+                                , vpFrameRate = 24
+                                }
+
+videoProfile1080p = VideoProfile { vpId = "1080p4800Kbps"
+                                , vpBandwith = 4800
+                                , vpWidth = 1920
+                                , vpHeight = 1080
+                                , vpFrameRate = 24
+                                }
+
+videoProfile720p = VideoProfile { vpId = "720p4800Kbps"
+                                , vpBandwith = 2400
+                                , vpWidth = 1280
+                                , vpHeight = 720
+                                , vpFrameRate = 24
+                                }
 
 class XmlConvertible t where
   toXml :: t -> Xml Elem
@@ -54,7 +99,7 @@ instance XmlConvertible MPD where
               <> xattr "xmlns" "urn:mpeg:dash:schema:mpd:2011"
               <> xattr "xsi:schemaLocation" "urn:mpeg:dash:schema:mpd:2011 http://standards.iso.org/ittf/PubliclyAvailableStandards/MPEG-DASH_schema_files/DASH-MPD.xsd"
               <> xattr "type" "dynamic"
-              <> xattr "availabilityStartTime" ((showT . availabilityStartTime) m)
+              <> xattr "availabilityStartTime" (availabilityStartTime m)
               <> xattr "timeShiftBufferDepth" "PT10M"
               <> xattr "minBufferTime" "PT10S"
               <> xattr "profiles" "urn:mpeg:dash:profile:isoff-live:2011"
@@ -63,20 +108,23 @@ instance XmlConvertible MPD where
 instance XmlConvertible Period where
   toXml p = xelem "Period" $
                   xattr "id" ((showT . perId) p)
-              <>  xattr "start" ((show . perStart) p)
+              <>  xattr "start" (perStart p)
               <#> map toXml (perAdaptionSets p)
 
 
 instance XmlConvertible AdaptationSet where
   toXml as = xelem "AdaptationSet" $
-                 xattr "mimeType" ((showT . asMimeType) as)
-              <> xattr "codecs" ((showT . asCodecs) as)
+                 xattr "mimeType" (vfMimeType $ asVideoFormat as)
+              <> xattr "codecs" (vfCodecs $ asVideoFormat as)
              <#> map toXml (asRepresentations as)
 
 instance XmlConvertible Representation where
   toXml r = xelem "Representation" $
-                 xattr "id" ((showT . repId) r)
-              <> xattr "bandwidth" ((showT . repBandwidth) r)
+                 xattr "id" (vpId $ repVideoProfile r)
+              <> xattr "bandwidth" ((showT . vpBandwith . repVideoProfile) r)
+              <> xattr "width" ((showT . vpWidth . repVideoProfile) r)
+              <> xattr "height" ((showT . vpHeight . repVideoProfile) r)
+              <> xattr "frameRate" ((showT . vpFrameRate . repVideoProfile) r)
              <#> toXml (repSegmentTemplate r)
 
 instance XmlConvertible SegmentTemplate where
